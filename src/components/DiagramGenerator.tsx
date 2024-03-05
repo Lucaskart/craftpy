04ae1ed1ca7fc3b02b2c11b9901260480bab7dd5
Lucas <lucasfraga10@hotmail.com>
2024-03-05 17:12:34 -0300

    Merge

--- src/components/DiagramGenerator.tsx
+++ src/components/DiagramGenerator.tsx
@@ -8,94 +8,94 @@ import { CodeIcon, DownloadIcon, GearIcon, QuestionMarkIcon, ReaderIcon } from '
 
 function DiagramGenerator() {
     const codeText = `
-        class Patient:
-            def __init__(self, id:int, examRequest:ExamRequest, name:str, sex:str, birth:date):
-                self.id = id
-                self.name = name
-                self.sex = sex
-                self.birth = birth
-                self.exameRequest = examRequest
-                
-            def calculateAge(void):
-                return void
+    class Patient:
+        def __init__(self, id:int, examRequest:ExamRequest, name:str, sex:str, birth:date):
+            self.id = id
+            self.name = name
+            self.sex = sex
+            self.birth = birth
+            self.exameRequest = examRequest
             
-            def keep(void):
-                return void
+        def calculateAge(void):
+            return void
+        
+        def keep(void):
+            return void
 
-            def select(void):
-                return void
+        def select(void):
+            return void
 
-        class Exam:
-            def __init__(self, examName:str, examRequestList:ExamRequest, recommendation:str):
-                self.examName = examName
-                self.recommendation = recommendation
-                self.examRequestList = examRequestList
+    class Exam:
+        def __init__(self, examName:str, examRequestList:ExamRequest, recommendation:str):
+            self.examName = examName
+            self.recommendation = recommendation
+            self.examRequestList = examRequestList
+        
+        def keep(void):
+            return void
+
+        def select(void):
+            return void
+        
+    class ExamRequest:
+        def __init__(self, issueDateTime:date, dateRealization:date, dateTimeCancellation:date, orderStatus:str, PDFFile:str):
+            self.issueDateTime = issueDateTime
+            self.dateRealization = dateRealization
+            self.dateTimeCancellation = dateTimeCancellation
+            self.orderStatus = orderStatus
+            self.PDFFile = PDFFile
+            self.doctor = Doctor(name = "Joao", numberCRM = "1234")
             
-            def keep(void):
-                return void
+        def issue(void):
+            return void
 
-            def select(void):
-                return void
-            
-        class ExamRequest:
-            def __init__(self, issueDateTime:date, dateRealization:date, dateTimeCancellation:date, orderStatus:str, PDFFile:str):
-                self.issueDateTime = issueDateTime
-                self.dateRealization = dateRealization
-                self.dateTimeCancellation = dateTimeCancellation
-                self.orderStatus = orderStatus
-                self.PDFFile = PDFFile
-                self.doctor = Doctor(name = "Joao", numberCRM = "1234")
-                
-            def issue(void):
-                return void
+        def select(void):
+            return void
 
-            def select(void):
-                return void
+        def registerExam(void):
+            return void
 
-            def registerExam(void):
-                return void
+        def cancelOrder(void):
+            return void
 
-            def cancelOrder(void):
-                return void
+        def viewPDF(void):
+            return void
+        
+    class ExamReport:
+        def __init__(self, description:str, issueDate:str, examRequest:ExamRequest, statusReport:str):
+            self.description = description
+            self.issueDate = issueDate
+            self.statusReport = statusReport
+            self.examRequest = examRequest
+            self.doctor = Doctor(name = "Joao", numberCRM = "1234")
 
-            def viewPDF(void):
-                return void
-            
-        class ExamReport:
-            def __init__(self, description:str, issueDate:str, examRequest:ExamRequest, statusReport:str):
-                self.description = description
-                self.issueDate = issueDate
-                self.statusReport = statusReport
-                self.examRequest = examRequest
-                self.doctor = Doctor(name = "Joao", numberCRM = "1234")
+        def issue(void):
+            return void
 
-            def issue(void):
-                return void
+        def select(void):
+            return void
 
-            def select(void):
-                return void
+        def review(void):
+            return void
 
-            def review(void):
-                return void
+    class Doctor:
+        def __init__(self, name:str, numberCRM:str):
+            self.name = name
+            self.numberCRM = numberCRM
+        
+        def issue(void):
+            return void
 
-        class Doctor:
-            def __init__(self, name:str, numberCRM:str):
-                self.name = name
-                self.numberCRM = numberCRM
-            
-            def issue(void):
-                return void
+        def select(void):
+            return void
+        
+    class Resident(Doctor):
+        def __init__(self, yearResidence:str):
+            self.yearResidence = yearResidence
 
-            def select(void):
-                return void
-            
-        class Resident(Doctor):
-            def __init__(self, yearResidence:str):
-                self.yearResidence = yearResidence
-
-        class Teacher(Doctor):
-            def __init__(self, academicTitle:str):
-                self.academicTitle = academicTitle
+    class Teacher(Doctor):
+        def __init__(self, academicTitle:str):
+            self.academicTitle = academicTitle
       `;
     const [code, setCode] = React.useState(codeText);
     const [manipulatedText, setManipulatedText] = useTextManipulation();
