17dff7e4a295ae100685cf4bde5b7d7ce19d8880
Lucas <lucasfraga10@hotmail.com>
2024-03-09 03:52:59 -0300

    Download Image and Text

--- src/components/DiagramGenerator.tsx
+++ src/components/DiagramGenerator.tsx
@@ -5,99 +5,99 @@ import CodeMirror from '@uiw/react-codemirror';
 import { javascript } from '@codemirror/lang-javascript';
 import { Box, Button, Grid, Flex, Heading, TextArea } from '@radix-ui/themes';
 import { CodeIcon, DownloadIcon, GearIcon, QuestionMarkIcon, ReaderIcon } from '@radix-ui/react-icons'
+import html2canvas from 'html2canvas';
 
 //Commit for deploy.
 
 function DiagramGenerator() {
-    const codeText = `
-    class Patient:
-        def __init__(self, id:int, examRequest:ExamRequest, name:str, sex:str, birth:date):
-            self.id = id
-            self.name = name
-            self.sex = sex
-            self.birth = birth
-            self.exameRequest = examRequest
-            
-        def calculateAge(void):
-            return void
+    const codeText = `class Patient:
+    def __init__(self, id:int, examRequest:ExamRequest, name:str, sex:str, birth:date):
+        self.id = id
+        self.name = name
+        self.sex = sex
+        self.birth = birth
+        self.exameRequest = examRequest
         
-        def keep(void):
-            return void
+    def calculateAge(void):
+        return void
+    
+    def keep(void):
+        return void
 
-        def select(void):
-            return void
+    def select(void):
+        return void
 
-    class Exam:
-        def __init__(self, examName:str, examRequestList:ExamRequest, recommendation:str):
-            self.examName = examName
-            self.recommendation = recommendation
-            self.examRequestList = examRequestList
+class Exam:
+    def __init__(self, examName:str, examRequestList:ExamRequest, recommendation:str):
+        self.examName = examName
+        self.recommendation = recommendation
+        self.examRequestList = examRequestList
+    
+    def keep(void):
+        return void
+
+    def select(void):
+        return void
+    
+class ExamRequest:
+    def __init__(self, issueDateTime:date, dateRealization:date, dateTimeCancellation:date, orderStatus:str, PDFFile:str):
+        self.issueDateTime = issueDateTime
+        self.dateRealization = dateRealization
+        self.dateTimeCancellation = dateTimeCancellation
+        self.orderStatus = orderStatus
+        self.PDFFile = PDFFile
+        self.doctor = Doctor(name = "Joao", numberCRM = "1234")
         
-        def keep(void):
-            return void
+    def issue(void):
+        return void
 
-        def select(void):
-            return void
-        
-    class ExamRequest:
-        def __init__(self, issueDateTime:date, dateRealization:date, dateTimeCancellation:date, orderStatus:str, PDFFile:str):
-            self.issueDateTime = issueDateTime
-            self.dateRealization = dateRealization
-            self.dateTimeCancellation = dateTimeCancellation
-            self.orderStatus = orderStatus
-            self.PDFFile = PDFFile
-            self.doctor = Doctor(name = "Joao", numberCRM = "1234")
-            
-        def issue(void):
-            return void
+    def select(void):
+        return void
 
-        def select(void):
-            return void
+    def registerExam(void):
+        return void
 
-        def registerExam(void):
-            return void
+    def cancelOrder(void):
+        return void
 
-        def cancelOrder(void):
-            return void
+    def viewPDF(void):
+        return void
+    
+class ExamReport:
+    def __init__(self, description:str, issueDate:str, examRequest:ExamRequest, statusReport:str):
+        self.description = description
+        self.issueDate = issueDate
+        self.statusReport = statusReport
+        self.examRequest = examRequest
+        self.doctor = Doctor(name = "Joao", numberCRM = "1234")
 
-        def viewPDF(void):
-            return void
-        
-    class ExamReport:
-        def __init__(self, description:str, issueDate:str, examRequest:ExamRequest, statusReport:str):
-            self.description = description
-            self.issueDate = issueDate
-            self.statusReport = statusReport
-            self.examRequest = examRequest
-            self.doctor = Doctor(name = "Joao", numberCRM = "1234")
+    def issue(void):
+        return void
 
-        def issue(void):
-            return void
+    def select(void):
+        return void
 
-        def select(void):
-            return void
+    def review(void):
+        return void
 
-        def review(void):
-            return void
+class Doctor:
+    def __init__(self, name:str, numberCRM:str):
+        self.name = name
+        self.numberCRM = numberCRM
+    
+    def issue(void):
+        return void
 
-    class Doctor:
-        def __init__(self, name:str, numberCRM:str):
-            self.name = name
-            self.numberCRM = numberCRM
-        
-        def issue(void):
-            return void
+    def select(void):
+        return void
+    
+class Resident(Doctor):
+    def __init__(self, yearResidence:str):
+        self.yearResidence = yearResidence
 
-        def select(void):
-            return void
-        
-    class Resident(Doctor):
-        def __init__(self, yearResidence:str):
-            self.yearResidence = yearResidence
-
-    class Teacher(Doctor):
-        def __init__(self, academicTitle:str):
-            self.academicTitle = academicTitle
+class Teacher(Doctor):
+    def __init__(self, academicTitle:str):
+        self.academicTitle = academicTitle
       `;
     const [code, setCode] = React.useState(codeText);
     const [manipulatedText, setManipulatedText] = useTextManipulation();
@@ -117,15 +117,39 @@ function DiagramGenerator() {
         setCode(val);
     }, []);
 
+    const handleDownloadImage = async () => {
+        const element = document.getElementById('print')!,
+        canvas = await html2canvas(element),
+        data = canvas.toDataURL('image/jpg'),
+        link = document.createElement('a');
+
+        link.href = data;
+        link.download = 'downloaded-image.jpg';
+
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+    };
+
+    const handleDownloadCode = async () => {
+        //const fileData = JSON.stringify(codeText);
+        const blob = new Blob([codeText], { type: "text/plain" });
+        const url = URL.createObjectURL(blob);
+        const link = document.createElement("a");
+        link.download = "user-info.txt";
+        link.href = url;
+        link.click();
+    };
+
     return (
         <Flex direction="column" gap="3">
             <Box position="sticky" style={{ background: 'var(--gray-a2)', borderRadius: 'var(--radius-3)' }} p="3" width="100%">
                 <Grid columns="3" gap="3">
                     <Flex gap="3" justify="start">
-                        <Button size="2">
+                        <Button size="2" onClick={handleDownloadCode}>
                             <CodeIcon width="16" height="16" /> Salvar Código
                         </Button>
-                        <Button size="2">
+                        <Button size="2" onClick={handleDownloadImage}>
                             <DownloadIcon width="16" height="16" /> Salvar Diagrama
                         </Button>
                     </Flex>
@@ -167,8 +191,8 @@ function DiagramGenerator() {
                         </Box>
                     </Flex>
                     <Flex>
-                        <Box width="100%">
-                            {manipulatedText != "" && <Graphviz options={{ width: "100%", zoom: true, onerror: handleError }} dot={manipulatedText} />}
+                        <Box id="print" width="100%">
+                            {manipulatedText != "" && <Graphviz options={{ height: 750, width: "100%", zoom: false, onerror: handleError }} dot={manipulatedText} />}
                         </Box>
                     </Flex>
                 </Grid>
