7778953550fb9904b0e6c9251381c23830e5a13f
Lucas <lucasfraga10@hotmail.com>
2024-04-08 18:07:32 -0300

    Página de Exemplos e Ajuda

--- src/components/Examples.tsx
+++ src/components/Examples.tsx
@@ -1,4 +1,4 @@
-import { Box, Flex, Grid, Heading, Text } from '@radix-ui/themes';
+import { Box, Card, Code, Flex, Grid, Heading, Text } from '@radix-ui/themes';
 import CodeMirror from '@uiw/react-codemirror';
 import { javascript } from '@codemirror/lang-javascript';
 
@@ -107,20 +107,136 @@ class Address():
             </Flex>
             <Flex direction="column" gap="3" justify="center" align="center">
                 <Heading mb="2" size="7">Exemplos</Heading>
-                <Text mb="4">A seguir temos alguns exemplos de códigos em Python prontos para mostrar como ocorre o processo de geração de diagramas.</Text>
-                
-                <Heading mb="2" size="5">Exemplo 1</Heading>
-                <Text mb="4">Realizar Exame</Text>
-                <Box width="100%">
-                    <CodeMirror
-                        value={example1}
-                        height="520px"
-                        width="100%"
-                        theme="dark"
-                        readOnly
-                        extensions={[javascript({ jsx: true })]}
-                    />
-                </Box>
+                <Text align="center" as="div">A seguir temos uma lista de intruções em código Python para gerar certos componentes em diagramas.</Text>
+                <Card style={{ maxWidth: 600 }}>
+                    <Flex gap="3" align="center">
+                        <Box>
+                            <Text size="2">Classe: </Text>
+                            <Text as="div" size="2" color="gray">
+                                Para a criação de uma classe no diagrama, é necessário iniciar uma classe Python.
+                            </Text>
+                            <Text as="div" size="2" color="gray">
+                                Para adicionar atributos a classe, é necessário criar um construtor para a classe através do método '__init__'.
+                            </Text>
+                            <Text as="div" size="2" color="gray">
+                                Para adicionar funções a classe, é necesário definir as funções dentro do escopo da classe.
+                            </Text>
+                            <Text as="div" size="2" color="gray">
+                                É possível criar atributos e métodos privados atráves de subtraços.
+                            </Text>
+                            <Text as="div" mt="2" size="2" color="gray">
+                                Exemplo:
+                            </Text>
+                            <Code size="2">
+                            class Carro:<br/>
+                            &nbsp; def __init__(self, marca:str):<br/>
+                            &nbsp;&nbsp; self.__marca = marca<br/>
+                            &nbsp;<br/>
+                            &nbsp; def aumentarKM(self):<br/>
+                            &nbsp;&nbsp; pass<br/>
+                            </Code>
+                        </Box>
+                    </Flex>
+                </Card>
+                <Card style={{ maxWidth: 600 }}>
+                    <Flex gap="3" align="center">
+                        <Box>
+                            <Text size="2">Associação: </Text>
+                            <Text as="div" size="2" color="gray">
+                                Para a criação de uma associação entre classes no diagrama, é necessário iniciar um objeto dentro de uma classe pertencente a outra classe 
+                                fora do construtor para evitar relações de dependencia.
+                            </Text>
+                            <Text as="div" size="2" color="gray">
+                                É possível criar multiplicidade atráves do uso de listas.
+                            </Text>
+                            <Text as="div" mt="2" size="2" color="gray">
+                                Exemplo:
+                            </Text>
+                            <Code size="2">
+                            class Carro:<br/>
+                            &nbsp; __bankacc: list[BankAccount] = list()<br/>
+                            </Code>
+                        </Box>
+                    </Flex>
+                </Card>
+                <Card style={{ maxWidth: 600 }}>
+                    <Flex gap="3" align="center">
+                        <Box>
+                            <Text size="2">Herança: </Text>
+                            <Text as="div" size="2" color="gray">
+                                É possível que uma classe herde de outra atráves do uso de parenteses quando ela é iniciada.
+                            </Text>
+                            <Text as="div" mt="2" size="2" color="gray">
+                                Exemplo:
+                            </Text>
+                            <Code size="2">
+                            class Carro(Automovel):<br/>
+                            </Code>
+                        </Box>
+                    </Flex>
+                </Card>
+                <Card style={{ maxWidth: 600 }}>
+                    <Flex gap="3" align="center">
+                        <Box>
+                            <Text size="2">Agregação: </Text>
+                            <Text as="div" size="2" color="gray">
+                                É possível criar uma agregação entre classes quando uma classe utiliza de objetos de outra classe em seu construtor.
+                            </Text>
+                            <Text as="div" mt="2" size="2" color="gray">
+                                Exemplo:
+                            </Text>
+                            <Code size="2">
+                            class Carro(Automovel):<br/>
+                            &nbsp; def __init__(self, marca:str, cor:str, rodas:list[Roda]):<br/>
+                            &nbsp;&nbsp; self.marca = marca<br/>
+                            &nbsp;&nbsp; self.cor:str = cor<br/>
+                            &nbsp;&nbsp; self.roda:list[Roda] = rodas<br/>
+                            </Code>
+                        </Box>
+                    </Flex>
+                </Card>
+                <Card style={{ maxWidth: 600 }}>
+                    <Flex gap="3" align="center">
+                        <Box>
+                            <Text size="2">Composição: </Text>
+                            <Text as="div" size="2" color="gray">
+                                É possível criar uma composição entre classes quando uma classe cria um objeto de outra classe em seu construtor.
+                            </Text>
+                            <Text as="div" mt="2" size="2" color="gray">
+                                Exemplo:
+                            </Text>
+                            <Code size="2">
+                            class Carro(Automovel):<br/>
+                            &nbsp; def __init__(self, marca:str, cor:str):<br/>
+                            &nbsp;&nbsp; self.marca = marca<br/>
+                            &nbsp;&nbsp; self.cor:str = cor<br/>
+                            &nbsp;&nbsp; self.propietario: Propietario = Propietario(name = "Joao")<br/>
+                            </Code>
+                        </Box>
+                    </Flex>
+                </Card>
+                <Card style={{ maxWidth: 600 }}>
+                    <Flex gap="3" align="center">
+                        <Box>
+                            <Text size="2">Exemplos Completos: </Text>
+                            <Text as="div" size="2" color="gray">
+                                A seguir temos alguns exemplos de códigos completos em Python prontos para demostrar como ocorre o processo de geração de diagramas.
+                            </Text>
+                            <Text as="div" mt="2" size="2" color="gray">
+                                Exemplo 1: Realizar Exame
+                            </Text>
+                            <Card style={{ maxWidth: 580 }}>
+                                <CodeMirror
+                                    value={example1}
+                                    height="520px"
+                                    theme="dark"
+                                    readOnly
+                                    extensions={[javascript({ jsx: true })]}
+                                />
+                            </Card>
+                        </Box>
+                    </Flex>
+                </Card>
             </Flex>
             <Flex gap="3">
             </Flex>
--- src/components/Help.tsx
+++ src/components/Help.tsx
@@ -1,9 +1,11 @@
-import { Box, Card, Code, Flex, Grid, Heading, Kbd, Link, Text } from '@radix-ui/themes';
+import { Box, Card, Flex, Grid, Heading, Kbd, Link, Text } from '@radix-ui/themes';
 
 function Help() {
 
     return (
-        <Grid columns="2" p="3">
+        <Grid columns="3" p="3">
+            <Flex gap="3">
+            </Flex>
             <Flex direction="column" gap="3" justify="start" align="center">
                 <Heading mb="2" size="7">Atalhos</Heading>
                 <Text align="center" as="div">A seguir temos uma lista de atalhos para auxiliar no uso de Py2UML. 
@@ -72,116 +74,7 @@ function Help() {
                     </Flex>
                 </Card>
             </Flex>
-            <Flex direction="column" gap="3" justify="center" align="center">
-                <Heading mb="2" size="7">Comandos</Heading>
-                <Text align="center" as="div">A seguir temos uma lista de intruções em código Python para gerar certos componentes em diagramas.</Text>
-                <Card style={{ maxWidth: 600 }}>
-                    <Flex gap="3" align="center">
-                        <Box>
-                            <Text size="2">Classe: </Text>
-                            <Text as="div" size="2" color="gray">
-                                Para a criação de uma classe no diagrama, é necessário iniciar uma classe Python.
-                            </Text>
-                            <Text as="div" size="2" color="gray">
-                                Para adicionar atributos a classe, é necessário criar um construtor para a classe através do método '__init__'.
-                            </Text>
-                            <Text as="div" size="2" color="gray">
-                                Para adicionar funções a classe, é necesário definir as funções dentro do escopo da classe.
-                            </Text>
-                            <Text as="div" size="2" color="gray">
-                                É possível criar atributos e métodos privados atráves de subtraços.
-                            </Text>
-                            <Text as="div" mt="2" size="2" color="gray">
-                                Exemplo:
-                            </Text>
-                            <Code size="2">
-                            class Carro:<br/>
-                            &nbsp; def __init__(self, marca:str):<br/>
-                            &nbsp;&nbsp; self.__marca = marca<br/>
-                            &nbsp;<br/>
-                            &nbsp; def aumentarKM(self):<br/>
-                            &nbsp;&nbsp; pass<br/>
-                            </Code>
-                        </Box>
-                    </Flex>
-                </Card>
-                <Card style={{ maxWidth: 600 }}>
-                    <Flex gap="3" align="center">
-                        <Box>
-                            <Text size="2">Associação: </Text>
-                            <Text as="div" size="2" color="gray">
-                                Para a criação de uma associação entre classes no diagrama, é necessário iniciar um objeto dentro de uma classe pertencente a outra classe 
-                                fora do construtor para evitar relações de dependencia.
-                            </Text>
-                            <Text as="div" size="2" color="gray">
-                                É possível criar multiplicidade atráves do uso de listas.
-                            </Text>
-                            <Text as="div" mt="2" size="2" color="gray">
-                                Exemplo:
-                            </Text>
-                            <Code size="2">
-                            class Carro:<br/>
-                            &nbsp; __bankacc: list[BankAccount] = list()<br/>
-                            </Code>
-                        </Box>
-                    </Flex>
-                </Card>
-                <Card style={{ maxWidth: 600 }}>
-                    <Flex gap="3" align="center">
-                        <Box>
-                            <Text size="2">Herança: </Text>
-                            <Text as="div" size="2" color="gray">
-                                É possível que uma classe herde de outra atráves do uso de parenteses quando ela é iniciada.
-                            </Text>
-                            <Text as="div" mt="2" size="2" color="gray">
-                                Exemplo:
-                            </Text>
-                            <Code size="2">
-                            class Carro(Automovel):<br/>
-                            </Code>
-                        </Box>
-                    </Flex>
-                </Card>
-                <Card style={{ maxWidth: 600 }}>
-                    <Flex gap="3" align="center">
-                        <Box>
-                            <Text size="2">Agregação e Composição: </Text>
-                            <Text as="div" size="2" color="gray">
-                                É possível criar uma agregação entre classes quando uma classe utiliza de objetos de outra classe em seu construtor.
-                            </Text>
-                            <Text as="div" mt="2" size="2" color="gray">
-                                Exemplo:
-                            </Text>
-                            <Code size="2">
-                            class Carro(Automovel):<br/>
-                            &nbsp; def __init__(self, marca:str, cor:str, rodas:list[Roda]):<br/>
-                            &nbsp;&nbsp; self.marca = marca<br/>
-                            &nbsp;&nbsp; self.cor:str = cor<br/>
-                            &nbsp;&nbsp; self.roda:list[Roda] = rodas<br/>
-                            </Code>
-                        </Box>
-                    </Flex>
-                </Card>
-                <Card style={{ maxWidth: 600 }}>
-                    <Flex gap="3" align="center">
-                        <Box>
-                            <Text size="2">Composição: </Text>
-                            <Text as="div" size="2" color="gray">
-                                É possível criar uma composição entre classes quando uma classe cria um objeto de outra classe em seu construtor.
-                            </Text>
-                            <Text as="div" mt="2" size="2" color="gray">
-                                Exemplo:
-                            </Text>
-                            <Code size="2">
-                            class Carro(Automovel):<br/>
-                            &nbsp; def __init__(self, marca:str, cor:str):<br/>
-                            &nbsp;&nbsp; self.marca = marca<br/>
-                            &nbsp;&nbsp; self.cor:str = cor<br/>
-                            &nbsp;&nbsp; self.propietario: Propietario = Propietario(name = "Joao")<br/>
-                            </Code>
-                        </Box>
-                    </Flex>
-                </Card>
+            <Flex gap="3">
             </Flex>
         </Grid>
     );
