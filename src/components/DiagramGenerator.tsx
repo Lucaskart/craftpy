6bf29377609336f91361fb569e6222dfb231280a
rafatosta <rafa.ecomp@gmail.com>
2024-04-09 16:07:33 -0300

    DropdownMenu for diagrams

--- src/components/DiagramGenerator.tsx
+++ src/components/DiagramGenerator.tsx
@@ -3,8 +3,8 @@ import usePythonCodeAnalyzer from '../hooks/usePythonCodeAnalyzer';
 import { Graphviz } from 'graphviz-react';
 import CodeMirror from '@uiw/react-codemirror';
 import { python } from '@codemirror/lang-python';
-import { Box, Button, Grid, Flex, Heading, TextArea } from '@radix-ui/themes';
-import { CodeIcon, DownloadIcon, GearIcon } from '@radix-ui/react-icons'
+import { Box, Button, Grid, Flex, Heading, TextArea, DropdownMenu } from '@radix-ui/themes';
+import { CodeIcon, DownloadIcon, CaretDownIcon, CaretUpIcon } from '@radix-ui/react-icons'
 import exampleList from '../utils/examples/code'
 import html2canvas from 'html2canvas';
 
@@ -13,25 +13,21 @@ import html2canvas from 'html2canvas';
 function DiagramGenerator() {
     let codeTextExample = exampleList[2].code;
 
-    const [codeText, setCodeText, classDiagram] = usePythonCodeAnalyzer(codeTextExample)
+    const [codeText, setCodeText, classDiagram/*, useCaseDiagram*/] = usePythonCodeAnalyzer(codeTextExample)
 
     const [logtext, setLogtext] = React.useState("");
+
+    //const [dotText, setDotText] = React.useState(classDiagram);
+
+    const [isOpen, setIsOpen] = React.useState(false);
+
+    const [selectedValue, setSelectedItem] = React.useState("Diagrama de Classe");
 
     const handleError = (errorMessage: string) => {
         //let line = errorMessage.replace(/.*error in line ([0–9]*) .*\n/, '$1');
         setLogtext(errorMessage);
         console.error(errorMessage)
     }
-
-    const handleButtonClick = () => {
-        if (codeText != "") {
-            //setManipulatedText(code);
-            setCodeText(codeText)
-
-        } else {
-            setLogtext("No code identified in the code field.")
-        }
-    };
 
     const onChange = React.useCallback((val: React.SetStateAction<string>) => {
         setCodeText(val);
@@ -61,6 +57,17 @@ function DiagramGenerator() {
         link.click();
     };
 
+
+    function setItem(e: Event) {
+        const target = e.target as HTMLDivElement;
+        setSelectedItem(target.textContent ?? "unknown");
+        /* if (target.textContent == "Diagrama de Classe") {
+            setDotText(classDiagram)
+        } else {
+            setDotText(useCaseDiagram)
+        } */
+    }
+
     return (
         <Flex direction="column" gap="3">
             <Box position="static" p="3" width="100%">
@@ -71,9 +78,17 @@ function DiagramGenerator() {
                         </Button>
                     </Flex>
                     <Flex gap="1" justify="center">
-                        <Button accessKey='2' size="2" onClick={handleButtonClick}>
-                            <GearIcon width="16" height="16" /> Compilar
-                        </Button>
+                        <DropdownMenu.Root onOpenChange={setIsOpen}>
+                            <DropdownMenu.Trigger>
+                                <Button variant="soft">
+                                    {selectedValue} {isOpen ? <CaretUpIcon /> : <CaretDownIcon />}
+                                </Button>
+                            </DropdownMenu.Trigger>
+                            <DropdownMenu.Content>
+                                <DropdownMenu.Item onSelect={setItem} accessKey='2'>Diagrama de Classe</DropdownMenu.Item>
+                                <DropdownMenu.Item onSelect={setItem}>Casos de Uso</DropdownMenu.Item>
+                            </DropdownMenu.Content>
+                        </DropdownMenu.Root>
                     </Flex>
                     <Flex gap="3" justify="end">
                         <Button accessKey='3' size="2" onClick={handleDownloadImage}>
