f12fc275188d6ec3dfe1755688b2c88ea2551b5d
rafatosta <rafa.ecomp@gmail.com>
2024-04-10 14:52:06 -0300

    add UseCaseDiagram Component

--- src/components/diagrams/UseCaseDiagram.tsx
+++ src/components/diagrams/UseCaseDiagram.tsx
@@ -1,5 +1,37 @@
-function UseCaseDiagram() {
-    return ( <div></div> );
+import { ClassInterface } from './../../hooks/usePythonCodeAnalyzer';
+import drawUseCaseDiagram from '../../utils/drawFunctions/drawUseCaseDiagram'
+
+import { useEffect, useState } from "react";
+import { select as d3Select } from 'd3-selection';
+import 'd3-graphviz';
+
+
+interface IProps {
+    classData: ClassInterface[];
+}
+
+const ID = 'graphUseCase'
+
+function UseCaseDiagram({ classData }: IProps) {
+
+    const [dotText, setDotText] = useState<string>('digraph {}');
+
+    useEffect(() => {
+        const dot = drawUseCaseDiagram(classData)
+        setDotText(dot)
+        createGraph();
+    }, [dotText, classData]);
+
+    const createGraph = () => {
+        d3Select<HTMLElement, any>(`#${ID}`)
+            .graphviz()
+            .addImage("stick.png", "40px", "100px")
+            .dot(dotText)
+            .render()
+    }
+
+    return (<div id={ID}>
+    </div>);
 }
 
 export default UseCaseDiagram;
---
+++ src/utils/drawFunctions/drawUseCaseDiagram.ts
@@ -0,0 +1,82 @@
+import { ClassInterface } from './../../hooks/usePythonCodeAnalyzer';
+
+function drawUseCaseDiagram(classes: ClassInterface[]): string {
+    let dot_content = ""
+
+    /* Actor Nodes */
+    dot_content += `node [shape=plaintext];\n`
+
+    classes.forEach((classe: ClassInterface) => {
+        dot_content += `subgraph cluster${classe.name} {label="${classe.name}";\n
+      ${classe.name}};\n${classe.name} [label="", image="stick.png"];\n`
+    })
+
+    /* Use Case Nodes */
+    dot_content += `node [shape=ellipse, style=solid];\n`
+
+    classes.forEach((classe: ClassInterface) => {
+        classe.functions.forEach((func) => {
+            func.decorators?.forEach((decorator) => {
+                console.log(decorator);
+                dot_content += `${func.name} [label="${func.name}"];\n`
+            })
+        })
+    })
+
+    /* Edges */
+    dot_content += `edge  [arrowhead="oarrow"];\n`
+
+    classes.forEach((classe: ClassInterface) => {
+        if (classe.inheritance) {
+            const superClasses = classe.inheritance.split(',')
+            for (var i = 0; i < superClasses.length; i++) {
+                dot_content += `${superClasses[i].trim()} -> ${classe.name};\n`
+            }
+        }
+    })
+
+    dot_content += `edge [arrowhead=none];\n`
+
+    classes.forEach((classe: ClassInterface) => {
+        classe.functions.forEach((func) => {
+            func.decorators?.forEach((decorator) => {
+                if (decorator.includes("@usecase")) {
+                    dot_content += `${classe.name} -> ${func.name};\n`
+                }
+            })
+        })
+    })
+
+    dot_content += `edge [arrowtail="vee", label="<<extend>>", style=dashed];\n`
+
+    classes.forEach((classe: ClassInterface) => {
+        classe.functions.forEach((func) => {
+            func.decorators?.forEach((decorator) => {
+                if (decorator.includes("@extends")) {
+                    dot_content += `${decorator.replace(/.*\[(.*?)\].*/, '$1')} -> ${func.name};\n`
+                }
+            })
+        })
+    })
+
+    dot_content += `edge [arrowtail="vee", label="<<include>>", style=dashed];\n`
+
+    classes.forEach((classe: ClassInterface) => {
+        classe.functions.forEach((func) => {
+            func.decorators?.forEach((decorator) => {
+                if (decorator.includes("@include")) {
+                    dot_content += `${decorator.replace(/.*\[(.*?)\].*/, '$1')} -> ${func.name};\n`
+                }
+            })
+        })
+    })
+
+
+    let dot_code = `digraph G {rankdir="LR";labelloc="b";peripheries=0;\n\n${dot_content}\n}`
+    console.log(dot_code);
+
+    return dot_code
+}
+
+
+export default drawUseCaseDiagram;
