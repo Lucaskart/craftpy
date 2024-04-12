b530b9ae51a95d2a4d2e579c32543a0aa36a1bdc
rafatosta <rafa.ecomp@gmail.com>
2024-04-12 17:17:17 -0300

    Update code.ts

--- src/utils/examples/code.ts
+++ src/utils/examples/code.ts
@@ -12,129 +12,6 @@ exampleList.push({
 })
 
 exampleList.push({
-    desciption: "Exemplo 1: Classe",
-    code: `
-class Carro:
-  def __init__(self, marca:str, chassi:str):
-    self.marca = marca #Atributo público
-    self.__chassi = marca #Atributo privado
-
-    def aumentarKM(self):
-        pass
-`})
-
-exampleList.push({
-    desciption: "Exemplo 2: Associação",
-    code: `
-class Aluno:
-    def __init__(self, nome:str, idade:int):
-        self.nome = nome
-        self.idade = idade
-
-    def info(self):
-        return f"Aluno: {self.nome}, Idade: {self.idade}"
-
-
-class Turma:
-    alunos:list[Aluno] = list() #Associação Simples
-    def __init__(self, nome_turma:str):
-        self.nome_turma = nome_turma
-
-    def adicionar_aluno(self, aluno):
-        self.alunos.append(aluno)
-
-    @usecase #Define o método como um Use Case para o Diagrama de Casos de Uso
-    def listar_alunos(self):
-        print(f"Alunos da turma {self.nome_turma}:")
-        for aluno in self.alunos:
-            print(aluno.info())
-`})
-
-exampleList.push({
-    desciption: "Exemplo 3: Herança",
-    code: `
-class Aluno:
-    def __init__(self, nome:str, idade:int):
-        self.nome = nome
-        self.idade = idade
-
-    def info(self):
-        return f"Aluno: {self.nome}, Idade: {self.idade}"
-
-
-class AlunoGraduacao(Aluno):
-    def __init__(self, nome:str, idade:str, curso:str):
-        super().__init__(nome, idade)
-        self.curso = curso
-
-    def info(self):
-        return f"Aluno de Graduação: {self.nome}, Idade: {self.idade}, Curso: {self.curso}"
-
-class AlunoMedioTecnico(Aluno):
-    def __init__(self, nome:str, idade:str, curso:str):
-        super().__init__(nome, idade)
-        self.curso = curso
-
-    def info(self):
-        return f"Aluno do Médio Técnico: {self.nome}, Idade: {self.idade}, Curso: {self.curso}"
-
-class Turma:
-    alunos:list[Aluno] = list() #Associação Simples
-    def __init__(self, nome_turma:str):
-        self.nome_turma = nome_turma
-    
-    @usecase
-    def adicionar_aluno(self, aluno):
-        self.alunos.append(aluno)
-    
-    @usecase
-    def listar_alunos(self):
-        print(f"Alunos da turma {self.nome_turma}:")
-        for aluno in self.alunos:
-            print(aluno.info())
-`})
-
-exampleList.push({
-    desciption: "Exemplo 4: Agregação",
-    code: `
-class Endereco:
-    def __init__(self, rua:str, numero:str, cidade:str, estado:str, cep:str):
-        self.rua = rua
-        self.numero = numero
-        self.cidade = cidade
-        self.estado = estado
-        self.cep = cep
-
-    def info(self):
-        return f"Endereço: {self.numero} {self.rua}, {self.cidade}, {self.estado}, CEP: {self.cep}"
-
-class Aluno:
-    #O(s) objeto(s) são passados por parâmetros pelo construtor
-    def __init__(self, nome:str, idade:intl, endereco:Endereco):
-        self.nome = nome
-        self.idade = idade
-        self.endereco = endereco 
-
-    def info(self):
-        return f"Aluno: {self.nome}, Idade: {self.idade}"
-
-
-class Turma:
-    def __init__(self, nome_turma:str, alunos:list[Aluno]):
-        self.nome_turma = nome_turma
-        self.alunos = alunos #O(s) objeto(s) são passados por parâmetros pelo construtor
-
-    def adicionar_aluno(self, aluno):
-        self.alunos.append(aluno)
-
-    @usecase #Define o método como um Use Case para o Diagrama de Casos de Uso
-    def listar_alunos(self):
-        print(f"Alunos da turma {self.nome_turma}:")
-        for aluno in self.alunos:
-            print(aluno.info())
-`})
-
-exampleList.push({
     desciption: "Sistema para vendas de ingressos de cinema", code: `class Filme:
     def __init__(self, titulo: str, duracao: int, genero: str):
         self.titulo = titulo  # Título do filme
@@ -283,6 +160,102 @@ class D:
         self.e = () #tuple
         self.f = {'Yan': '1234-5678', 'Pedro': '9999-9999'} #dict
         self.g = {} #dict
+`})
+
+exampleList.push({desciption:"Associação simples no Py2UML",code:`""" 
+Exemplo de Associação simples entre classes no Py2UML 
+"""
+
+class A:
+    def __init__(self):
+        pass
+
+class B:
+    def __init__(self):
+        pass
+
+class C:
+    def __init__(self):
+        pass
+
+class D:
+    a:A #Associação simples
+    b:B = none #Associação simples
+    c:list[C] = list() # Associação simples com multiplicidade
+    def __init__(self):
+        pass
+`})
+
+exampleList.push({desciption:"Herança no Py2UML",code:`""" 
+Exemplo de Herança entre classes no Py2UML 
+"""
+
+class A:
+    def __init__(self):
+        pass
+
+class B:
+    def __init__(self):
+        pass
+
+class C(A): #Herança única
+    def __init__(self):
+        pass
+
+class D(A,B): #Herança múltipla
+    def __init__(self):
+        pass
+`})
+
+exampleList.push({desciption:"Agregação no Py2UML",code:`""" 
+Exemplo de Agregação entre classes no Py2UML 
+"""
+
+class A:
+    def __init__(self):
+        pass
+
+class B:
+    def __init__(self):
+        pass
+
+class C:
+    def __init__(self, a:A):
+        self.a = a #Agregação
+
+class D:
+    def __init__(self, a:list[A], b):
+        self.a = a #Agregação com multiplicidade
+        self.b:list[B] = b #Agregação com multiplicidade
+`})
+
+exampleList.push({desciption:"Composição no Py2UML",code:`""" 
+Exemplo de Composição entre classes no Py2UML 
+"""
+
+class A:
+    def __init__(self):
+        pass
+
+class B:
+    def __init__(self):
+        self.a = A() #Composição
+`})
+
+exampleList.push({desciption:"Dependência no Py2UML",code:`""" 
+Exemplo de Dependência entre classes no Py2UML 
+"""
+
+class A:
+    def __init__(self):
+        pass
+
+class B:
+    def __init__(self):
+        pass
+    
+    def dependency_method(self):
+        self.a = A() #Dependência (create)
 `})
 
 //exampleList.push({desciption:"",code:})
